979d8344f84ebcf5320dab28050b012b7019db3d
Bernardash <bernard.programmer@gmail.com>
2026-06-04 11:49:44 -0300

    Incluindo exemplo de bibliotecas no python e adicionando um gitignore

---
+++ minha_biblioteca.py
@@ -0,0 +1,13 @@
+# Função
+def saudacao(nome):
+    return f"Olá, {nome}!"
+
+
+# Classe
+class Calculadora:
+
+    def somar(self, a, b):
+        return a + b
+
+    def multiplicar(self, a, b):
+        return a * b
